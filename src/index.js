c38d86ab2b802ac4b962ad721c54c6c9f2675568
Kyaw Ko Ko <kyawkoko176@gmail.com>
2025-04-15 12:42:06 +0630

    Initial commit 💖

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoveClicker from "./LoveClicker";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LoveClicker />);
+
